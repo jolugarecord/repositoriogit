9ee039ed70895fdaf2ea8716d92a20fee3e9e222
jolugarecord <jolugarecord@gmail.com>
2023-01-28 17:58:06 -0300

    actualizacion

--- .vscode/jss/funcion.js
+++
@@ -1,4 +0,0 @@
-document.getElementsByid("boton").onclic=function(){
-    console.log("capturamos el evento clic");
-    document.getelementbyid("demo").innertHTML ="estamos probando nuestro primer evento en js";
-}
